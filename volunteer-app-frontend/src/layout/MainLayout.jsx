617a0858023935325f5d05c77536a51a8ace7a4e
sandra-yelsukova <sandra.yelsukova@gmail.com>
2026-01-05 14:35:57 +0500

    edit MainLayout

--- volunteer-app-frontend/src/layout/MainLayout.jsx
+++ volunteer-app-frontend/src/layout/MainLayout.jsx
@@ -4,26 +4,26 @@ import { Outlet, Link } from 'react-router-dom';
 export default function MainLayout() {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar position="static" sx={{ borderRadius: 3, overflow: 'hidden', height: 72 }}>
+        <Toolbar sx={{ height: '100%', display: 'flex', alignItems: 'center', }}>
 
-          <Typography variant="h6" component={Link} to="/" sx={{ textDecoration: 'none', color: 'white', mr: 4 }}>
+          <Typography variant="h5" component={Link} to="/" sx={{ textDecoration: 'none', color: 'white', mr: 4 }}>
             Дашборд
           </Typography>
 
-          <Button color="inherit" component={Link} to="/projects">
+          <Button color="inherit" sx={{ fontSize: '1rem' }} component={Link} to="/projects">
             Проекты
           </Button>
-          <Button color="inherit" component={Link} to="/participants">
+          <Button color="inherit" sx={{ fontSize: '1rem' }} component={Link} to="/participants">
             Участники
           </Button>
-          <Button color="inherit" component={Link} to="/reports">
+          <Button color="inherit" sx={{ fontSize: '1rem' }} component={Link} to="/reports">
             Отчёты
           </Button>
 
           <Box sx={{ flexGrow: 1 }} />
 
-          <Button color="inherit" component={Link} to="/profile">
+          <Button color="inherit" component={Link} sx={{ fontSize: '1rem' }} to="/profile">
             Профиль
           </Button>
         </Toolbar>
